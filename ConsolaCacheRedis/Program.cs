793abfe721a096f6fef3104978bd370807e8179c
xiangcitoZ <pzxiang1@gmail.com>
2023-04-25 14:21:51 +0200

    Add project files.

---
+++ ConsolaCacheRedis/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsolaCacheRedis.Models;
+using ConsolaCacheRedis.Services;
+
+namespace ConsolaCacheRedis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cliente Movil de Azure Cache Redis");
+            ServiceCacheRedis service = new ServiceCacheRedis();
+
+
+            List<Producto> productos = service.GetProductosFavoritos();
+            if(productos == null ) 
+            {
+                Console.WriteLine("No existen favoritos");
+            
+            }
+            else
+            {
+                int i = 1;
+                foreach(Producto p in productos) 
+                {
+                    Console.WriteLine(i + ".- " + p.Nombre);
+                    i++;
+                }
+            }
+
+            Console.WriteLine("Pulse ENTER para finalizar");
+            Console.ReadLine();
+
+        }
+    }
+}
